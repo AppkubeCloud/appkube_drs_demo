b2fd78b6f495905e9ec42645f6a50b8a954c6170
Ubuntu <ubuntu@ip-172-31-93-190.ec2.internal>
2023-11-08 16:54:10 +0000

    Adding FailBack Config Init and Config Sync

---
+++ fb-chefconfig_sync.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ "$#" -lt 1 ]; then
+  echo 'Required 1 parameter, usage:- ./chefconfig_sync.sh git_repo_path'
+  exit 1
+fi
+repopath=$1
+# Define the local repository path
+repo_path="$repopath"
+# Change to the local repository directory
+cd "$repo_path" || exit
+    echo "Synchronization of cookbooks,databags and roles..."
+    # pull changes from the remote master branch
+    git pull origin main
+    # upload the cookbook
+    knife cookbook upload --cookbook-path "$repo_path"/cookbooks/ --all
+    # create pr data bag
+    knife data bag create configbag
+    # import pr data bags
+    knife data bag from file configbag --all
+    # create dr data bag
+    knife data bag create drconfigbag
+    # import dr data bags
+    knife data bag from file drconfigbag --all
+    # import role
+    knife role from file roles/*.json
+    # Attach pr node to role
+    knife node run_list add DBNode role['pr_role']
+    knife node run_list add WebNode role['pr_role']
+    knife node run_list add AppNode role['pr_role']
+    exit 0
